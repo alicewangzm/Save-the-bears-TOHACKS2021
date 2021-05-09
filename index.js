6dc6661028b1adeec4e11644c962c382ddc3977e
Xintong Li <55467454+XintongLi1@users.noreply.github.com>
2021-05-09 12:41:12 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+var country = "Canada";
+var background_jpg = "";
+
+function getCountry() {
+    country = document.getElementById("Country").value;
+    localStorage.setItem("myCountry", country);
+    background_jpg = null;
+    switch (country) {
+        case 'Canada':
+            background_jpg = ""
+            break;
+        case 'China':
+            background_jpg = ""
+            break;
+        case 'Egypt':
+            background_jpg = ""
+            break;
+        default:
+            background_jpg = ""
+            break;
+    }
+    location.href = "bear.html";
+    localStorage.setItem("refresh", "true");
+}
+
+function loadCountry() {
+    country = localStorage.getItem("myCountry");
+    var showCountry = document.createElement("showContry");
+    showCountry.innerHTML = country;
+    showCountry.style = "font-family: Raleway, sans-serif;"
+    document.getElementById("title").appendChild(showCountry);
+}
+
+
+function moveForward(){    
+    progress = parseInt(localStorage.getItem("currentProgress"))
+    localStorage.setItem("currentProgress", progress + 1)
+    if (localStorage.getItem("currentProgress") >= number_of_questions){
+        final_score()
+    }
+}
+
+function makeProgress() {
+    
+    progress = parseInt(localStorage.getItem("currentProgress"))
+    percentage = Math.round(progress / number_of_questions * 100)
+
+    $(".progress-bar").css("width", percentage + "%").text(percentage + " %");
+        
+    //setTimeout("makeProgress()", 500);
+}
+
+
+function final_score(){
+    location.href = "final.html";
+}
+
+function update_score(){
+    score_node = document.createElement("score_node")
+    score_node.innerHTML = "Score: " + localStorage.getItem("score")
+    score = document.getElementById("score")
+    while (score.firstChild){
+        score.removeChild(score.firstChild)
+    }
+    score.appendChild(score_node)
+}
